173d6038565d626d00a0a17a293e5c92538099aa
Udith Supeshala <supeshalagunasekara@gmail.com>
2024-02-28 23:29:59 +0530

    push commit passed

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("LOL");
+  int x,y;
+  printf("Enter Two Values: ");
+  scanf("%d %d", &x, &y);
+
+  printf("Sum is: %d", x+y);
+  printf("Avg is: %d", ((x+y)/2));
+  
+  return 0;
+}
